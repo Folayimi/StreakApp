828d82e16d8b6655b1e22755fc30d809cb9124ac
Ridwan <ridwanfolayimi@gmail.com>
2022-10-16 18:50:51 +0100

    created the model

--- Backend/Flaskr/__init__.py
+++ Backend/Flaskr/__init__.py
@@ -5,4 +5,29 @@ from flask import (
     jsonify
 )
 from flask_cors import *
+from models import setup_db
 
+def create_app():
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers', 'Content-Type, Authoriztion'
+        )
+        response.headers.add(
+            'Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS'
+        )
+        response.headers.add(
+            'Access-Control-Allow-Credentials', 'true'
+        )
+        return response
+
+    @cross_origin
+    @app.route('/get',methods=['GET'])
+    def get():
+
+
+    
--- Backend/models.py
+++ Backend/models.py
@@ -1,8 +1,17 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, create_engine
+import json
 
+database_name = "streak"
+database_password = os.environ.get('DATABASE_PASSWORD')
+database_path = "postgresql://{}:{}@{}/{}".format('postgres',database_password,'localhost:5000',database_name)
 
 db = SQLAlchemy()
 
-def setup_db(app):
+def setup_db(app, database_path=database_path,):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHYMI_TRACK_MODIFICATIONS"] = False
     db.app = app
+    db.init_app(app)
+    db.create_all()
